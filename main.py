f64967c6d731a11259d7cd704b55cda4105ee714
EvilScott <reis.robert.s@gmail.com>
2021-12-12 18:36:40 -0800

    break up main so its easier to apply conditionals

--- main.py
+++ main.py
@@ -1,38 +1,65 @@
+import argparse
 import sedna.athena as athena
 import sedna.iam as iam
 import sedna.kms as kms
 import sedna.rds as rds
 
 
-def main():
-    # prerequisites
+def check_prereqs():
     # TODO check for version
     # TODO check for env
     # TODO check for basic AWS access
     # TODO check for existence of S3
+    pass
 
-    # snapshot and export iam
-    export_policy = iam.get_or_create_export_policy()
-    export_role = iam.get_or_create_export_role()
-    iam.attach_export_policy_to_role(export_policy)
 
-    # snapshot export
-    key_id = kms.get_or_create_export_key()
-    snapshot_arn = rds.get_or_create_snapshot()
-    rds.get_or_create_export(snapshot_arn, export_role.name, key_id)
+def permissions():
+    # export_policy = iam.get_or_create_export_policy()
+    # export_role = iam.get_or_create_export_role()
+    # iam.attach_export_policy_to_role(export_policy)
+    pass
 
-    # view export
-    rds_to_s3_policy = iam.get_or_create_rds_to_s3_policy()
-    role = iam.get_or_create_rds_to_s3_role()
-    iam.attach_rds_to_s3_policy_to_role(rds_to_s3_policy)
-    rds.attach_rds_to_s3_role_to_db(role)
-    rds.export_views()
 
-    # athena setup
-    athena.create_database()
-    athena.create_tables()
-    athena.test_tables()
-    exit(0)
+def snapshot_export():
+    # key_id = kms.get_or_create_export_key()
+    # snapshot_arn = rds.get_or_create_snapshot()
+    # rds.get_or_create_export(snapshot_arn, export_role.name, key_id)
+    pass
+
+
+def view_export():
+    # rds_to_s3_policy = iam.get_or_create_rds_to_s3_policy()
+    # role = iam.get_or_create_rds_to_s3_role()
+    # iam.attach_rds_to_s3_policy_to_role(rds_to_s3_policy)
+    # rds.attach_rds_to_s3_role_to_db(role)
+    # rds.export_views()
+    pass
+
+
+def test_tables():
+    # athena.test_tables()
+    pass
+
+
+def aggregation():
+    # TODO
+    pass
+
+
+def allocation():
+    # TODO
+    pass
+
+
+def main():
+    # TODO handle args with argparse
+    check_prereqs()
+    permissions()
+    snapshot_export()
+    view_export()
+    test_tables()
+    aggregation()
+    allocation()
 
 
 if __name__ == '__main__':
