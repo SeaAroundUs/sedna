5ea1ac967d9f841127ff02d1a52fef542d97f3c1
EvilScott <reis.robert.s@gmail.com>
2022-05-16 16:04:26 -0700

    fill in nafo, ices, and ccamlr area_id subqueries

--- sql/create_data_table.sql
+++ sql/create_data_table.sql
@@ -1,37 +1,10 @@
--- old logic:
--- loop over fishing entities from dataraw
-    -- verify dataraw
-        -- gonna wait on this for now
-    -- import dataraw
-        -- the action happens here:
-        -- https://github.com/SeaAroundUs/MerlinCSharp_MSSQL/blob/a90ee38b5b9fc7827803b6a267c4e681a764bfa2/IntegrateDataRaw/ImportDataRaw.cs#L15
-
--- proposed new logic:
--- verify in separate step
--- order by fishing entity and layer (is this needed?)
--- do it in a big athena CTAS query
-
 -- data table shape: (from https://github.com/SeaAroundUs/MerlinCSharp_MSSQL/blob/a90ee38b5b9fc7827803b6a267c4e681a764bfa2/IntegrateDataRaw/ImportDataRaw.cs#L71)
 -- area assignment nonsense handled here:
 -- https://github.com/SeaAroundUs/MerlinCSharp_MSSQL/blob/a90ee38b5b9fc7827803b6a267c4e681a764bfa2/Area/AssignGenericAreaIDToData.cs#L9
 -- its a conditional on spatial type which is determined via this enum:
 -- https://github.com/SeaAroundUs/MerlinCSharp_MSSQL/blob/a90ee38b5b9fc7827803b6a267c4e681a764bfa2/Factory/ResolveSpatialType.cs
 
---TODO maybe skip layer 3 for now as those seem super complicated? also no layer 3 in test data
-
---TODO is this worth putting into dataraw as a row?
---         public static bool IsIFA(DataRaw dr)
---         {
---             byte? sectorID = SectorTypeCollection.GetInstance().GetSectorTypeID(dr.Sector);
---             if (sectorID == null)
---                 throw new Exception("Sector = " + dr.Sector + " is undefined");
---             if (dr.DataLayerID == 1 && sectorID.Value != 1 && (dr.EEZID > 0))
---                 return true;
---             else if (dr.DataLayerID == 3 && sectorID.Value != 1)
---                 return true;
---             else
---                 return false;
---         }
+--TODO maybe skip layer 3 for now as those seem super complicated? also no layer 3 in test data?
 
 -- test query finished REALLY fast (3s) with 100_000 rows of dataraw (leaving Layer3, NAFO, ICES, and CCAMLR null)
 -- test query of full dataraw 13_979_347 rows finished in just under a minute! (leaving Layer3, NAFO, ICES, and CCAMLR null)
@@ -43,25 +16,37 @@ SELECT universal_data_id,
            WHEN eez_id = 999 -- OR (dr.BigCellID != null && dr.BigCellID > 0) --TODO where is this field coming from?
            THEN NULL --TODO https://github.com/SeaAroundUs/MerlinCSharp_MSSQL/blob/a90ee38b5b9fc7827803b6a267c4e681a764bfa2/Area/AssignGenericAreaIDToData.cs#L129
            -- NAFO
-           WHEN fao_area_id = 21 AND nafo_division IS NULL
+           WHEN fao_area_id = 21 AND nafo_division IS NOT NULL
            THEN (SELECT allocation_simple_area_id
                  FROM allocation_simple_area asa
                  WHERE asa.marine_layer_id = 18
-                   AND asa.area_id = NULL --TODO EEZ_HighSea_NAFO_ComboCollection.GetInstance().GetDetails(dataRaw.EEZID, dataRaw.NAFODivision, isIFA()).EEZ_NAFO_ComboID
+                   AND asa.area_id = (SELECT eez_nafo_combo_id
+                                      FROM eez_nafo_combo enc
+                                      WHERE enc.eez_id = dr.eez_id
+                                        AND enc.nafo_division = dr.nafo_division
+                                        AND enc.is_ifa = (st.sector_type_id != 1))
                    AND asa.fao_area_id = dr.fao_area_id)
            -- CCAMLR
            WHEN fao_area_id IN (48, 58, 88) AND ccamlr_area IS NOT NULL
            THEN (SELECT allocation_simple_area_id
                  FROM allocation_simple_area asa
                  WHERE asa.marine_layer_id = 17
-                   AND asa.area_id = NULL --TODO EEZ_HighSea_CCAMLR_ComboCollection.GetInstance().GetDetails(dataRaw.EEZID, dataRaw.CCAMLRArea, isIFA()).EEZ_CCAMLAR_ComboID
+                   AND asa.area_id = (SELECT eez_ccamlar_combo_id
+                                      FROM eez_ccamlr_combo ecc
+                                      WHERE ecc.eez_id = dr.eez_id
+                                        AND ecc.ccamlr_area_id = dr.ccamlr_area
+                                        AND ecc.is_ifa = (st.sector_type_id != 1))
                    AND asa.fao_area_id = dr.fao_area_id)
            -- ICES
            WHEN fao_area_id = 27
            THEN (SELECT allocation_simple_area_id
                  FROM allocation_simple_area asa
                  WHERE asa.marine_layer_id = 15
-                   AND asa.area_id = NULL --TODO EEZ_HighSea_ICES_ComboCollection.GetInstance().GetDetails(dataRaw.EEZID, dataRaw.ICES_AreaID, isIFA()).EEZ_ICES_ComboID
+                   AND asa.area_id = (SELECT eez_ices_combo_id
+                                      FROM eez_ices_combo eic
+                                      WHERE eic.eez_id = dr.eez_id
+                                        AND eic.ices_area_id = dr.ices_area
+                                        AND eic.is_ifa = (st.sector_type_id != 1))
                    AND asa.fao_area_id = dr.fao_area_id)
            -- High Seas
            WHEN eez_id = 0 AND fao_area_id > 0
@@ -91,10 +76,10 @@ SELECT universal_data_id,
        reporting_status_id,
        layer AS data_layer_id,
        gear_type_id,
-       input_type_id,
-       sector_type_id,
+       it.input_type_id,
+       st.sector_type_id,
        taxon_key,
        year
 FROM dataraw dr
-JOIN input_type ON (dr.input = input_type.name)
-JOIN sector_type ON (dr.sector = sector_type.name);
+JOIN input_type it ON (dr.input = input_type.name)
+JOIN sector_type st ON (dr.sector = sector_type.name);
