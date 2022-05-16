cd8a307c87ee581e3237f2940e6eab911f14d355
EvilScott <reis.robert.s@gmail.com>
2022-05-16 16:34:55 -0700

    move area_id logic to dataraw table to address nested subqueries

--- sql/create_data_table.sql
+++ sql/create_data_table.sql
@@ -1,14 +1,6 @@
--- data table shape: (from https://github.com/SeaAroundUs/MerlinCSharp_MSSQL/blob/a90ee38b5b9fc7827803b6a267c4e681a764bfa2/IntegrateDataRaw/ImportDataRaw.cs#L71)
--- area assignment nonsense handled here:
--- https://github.com/SeaAroundUs/MerlinCSharp_MSSQL/blob/a90ee38b5b9fc7827803b6a267c4e681a764bfa2/Area/AssignGenericAreaIDToData.cs#L9
--- its a conditional on spatial type which is determined via this enum:
--- https://github.com/SeaAroundUs/MerlinCSharp_MSSQL/blob/a90ee38b5b9fc7827803b6a267c4e681a764bfa2/Factory/ResolveSpatialType.cs
-
---TODO maybe skip layer 3 for now as those seem super complicated? also no layer 3 in test data?
-
--- test query finished REALLY fast (3s) with 100_000 rows of dataraw (leaving Layer3, NAFO, ICES, and CCAMLR null)
--- test query of full dataraw 13_979_347 rows finished in just under a minute! (leaving Layer3, NAFO, ICES, and CCAMLR null)
-
+-- data table creation
+-- from https://github.com/SeaAroundUs/MerlinCSharp_MSSQL/blob/a90ee38b5b9fc7827803b6a267c4e681a764bfa2/IntegrateDataRaw/ImportDataRaw.cs#L71
+-- (had to move some of the area_id logic to the dataraw table due to nested sub-queries)
 SELECT universal_data_id,
        IF(eez_id = 999, 2, 1) AS allocation_area_type_id,
        CASE
@@ -18,56 +10,46 @@ SELECT universal_data_id,
            -- NAFO
            WHEN fao_area_id = 21 AND nafo_division IS NOT NULL
            THEN (SELECT allocation_simple_area_id
-                 FROM allocation_simple_area asa
+                 FROM sedna.allocation_simple_area asa
                  WHERE asa.marine_layer_id = 18
-                   AND asa.area_id = (SELECT eez_nafo_combo_id
-                                      FROM eez_nafo_combo enc
-                                      WHERE enc.eez_id = dr.eez_id
-                                        AND enc.nafo_division = dr.nafo_division
-                                        AND enc.is_ifa = (st.sector_type_id != 1))
+                   AND asa.area_id = dr.area_id
                    AND asa.fao_area_id = dr.fao_area_id)
            -- CCAMLR
            WHEN fao_area_id IN (48, 58, 88) AND ccamlr_area IS NOT NULL
            THEN (SELECT allocation_simple_area_id
-                 FROM allocation_simple_area asa
+                 FROM sedna.allocation_simple_area asa
                  WHERE asa.marine_layer_id = 17
-                   AND asa.area_id = (SELECT eez_ccamlar_combo_id
-                                      FROM eez_ccamlr_combo ecc
-                                      WHERE ecc.eez_id = dr.eez_id
-                                        AND ecc.ccamlr_area_id = dr.ccamlr_area
-                                        AND ecc.is_ifa = (st.sector_type_id != 1))
+                   AND asa.area_id = dr.area_id
                    AND asa.fao_area_id = dr.fao_area_id)
            -- ICES
            WHEN fao_area_id = 27
            THEN (SELECT allocation_simple_area_id
-                 FROM allocation_simple_area asa
+                 FROM sedna.allocation_simple_area asa
                  WHERE asa.marine_layer_id = 15
-                   AND asa.area_id = (SELECT eez_ices_combo_id
-                                      FROM eez_ices_combo eic
-                                      WHERE eic.eez_id = dr.eez_id
-                                        AND eic.ices_area_id = dr.ices_area
-                                        AND eic.is_ifa = (st.sector_type_id != 1))
+                   AND asa.area_id = dr.area_id
                    AND asa.fao_area_id = dr.fao_area_id)
            -- High Seas
            WHEN eez_id = 0 AND fao_area_id > 0
-           THEN (SELECT allocation_simple_area_id FROM allocation_simple_area asa
+           THEN (SELECT allocation_simple_area_id
+                 FROM sedna.allocation_simple_area asa
                  WHERE asa.marine_layer_id = 2
-                   AND asa.area_id = dr.fao_area_id
+                   AND asa.area_id = dr.area_id
                    AND asa.fao_area_id = dr.fao_area_id)
            -- IFA
-           WHEN eez_id > 0 AND fao_area_id > 0 AND sector_type_id != 1
-           THEN (SELECT allocation_simple_area_id FROM allocation_simple_area asa
+           WHEN dr.area_type = 'IFA'
+           THEN (SELECT allocation_simple_area_id
+                 FROM sedna.allocation_simple_area asa
                  WHERE asa.marine_layer_id = 14
-                   AND asa.area_id = dr.eez_id
+                   AND asa.area_id = dr.area_id
                    AND asa.fao_area_id = dr.fao_area_id)
            -- EEZ
-           WHEN eez_id > 0 AND fao_area_id > 0 AND sector_type_id = 1
-           THEN (SELECT allocation_simple_area_id FROM allocation_simple_area asa
+           WHEN dr.area_type = 'EEZ'
+           THEN (SELECT allocation_simple_area_id
+                 FROM sedna.allocation_simple_area asa
                  WHERE asa.marine_layer_id = 12
-                   AND asa.area_id = dr.eez_id
+                   AND asa.area_id = dr.area_id
                    AND asa.fao_area_id = dr.fao_area_id)
-           -- this shouldn't happen; check for these
-           ELSE NULL
+           ELSE NULL -- this shouldn't happen; check for these
        END AS generic_allocation_area_id,
        fishing_entity_id AS original_fishing_entity_id,
        fishing_entity_id AS fishing_entity_id, --TODO 213 if it needs to be reassigned to unknown
@@ -80,6 +62,6 @@ SELECT universal_data_id,
        st.sector_type_id,
        taxon_key,
        year
-FROM dataraw dr
-JOIN input_type it ON (dr.input = it.name)
-JOIN sector_type st ON (dr.sector = st.name);
+FROM sedna.dataraw dr
+JOIN sedna.input_type it ON (dr.input = it.name)
+JOIN sedna.sector_type st ON (dr.sector = st.name);
--- sql/create_dataraw_table.sql
+++ sql/create_dataraw_table.sql
@@ -1,5 +1,7 @@
 -- dataraw table creation
 -- from https://github.com/SeaAroundUs/Merlin-database-mssql/blob/4b223108bad7e6863e7feae853053778026568c8/sprocs.sql#L1515
+-- had to bring some of the area_id logic over from the data table to handle nested sub-queries in athena
+-- from https://github.com/SeaAroundUs/MerlinCSharp_MSSQL/blob/a90ee38b5b9fc7827803b6a267c4e681a764bfa2/Area/AssignGenericAreaIDToData.cs#L30
 CREATE TABLE IF NOT EXISTS sedna.dataraw
 WITH (
   external_location = 's3://{BUCKET_NAME}/{PARQUET_PREFIX}/ctas.dataraw',
@@ -23,7 +25,53 @@ AS SELECT
 	c.gear_type_id,
 	ia.ices_area,
 	c.ccamlr_area,
-	na.nafo_division
+	na.nafo_division,
+    CASE
+        WHEN c.eez_id = 999 THEN 'Layer3'
+        WHEN c.fao_area_id = 21 AND na.nafo_division IS NOT NULL THEN 'NAFO'
+        WHEN c.fao_area_id IN (48, 58, 88) AND c.ccamlr_area IS NOT NULL THEN 'CCAMLR'
+        WHEN c.fao_area_id = 27 THEN 'ICES'
+        WHEN c.eez_id = 0 AND c.fao_area_id > 0 THEN 'High Seas'
+        WHEN c.eez_id > 0 AND c.fao_area_id > 0 AND c.sector_type_id != 1 THEN 'IFA'
+        WHEN c.eez_id > 0 AND c.fao_area_id > 0 AND c.sector_type_id = 1 THEN 'EEZ'
+        ELSE NULL -- this shouldn't happen
+    END AS area_type,
+    CASE
+        -- Layer3
+        WHEN c.eez_id = 999
+        THEN NULL --TODO
+        -- NAFO
+        WHEN c.fao_area_id = 21 AND na.nafo_division IS NOT NULL
+        THEN (SELECT eez_nafo_combo_id
+              FROM sedna.eez_nafo_combo enc
+              WHERE enc.eez_id = c.eez_id
+                AND enc.nafo_division = na.nafo_division
+                AND enc.is_ifa = (st.sector_type_id != 1))
+        -- CCAMLR
+        WHEN c.fao_area_id IN (48, 58, 88) AND c.ccamlr_area IS NOT NULL
+        THEN (SELECT eez_ccamlar_combo_id
+              FROM sedna.eez_ccamlr_combo ecc
+              WHERE ecc.eez_id = c.eez_id
+                AND ecc.ccamlr_area_id = c.ccamlr_area
+                AND ecc.is_ifa = (st.sector_type_id != 1))
+        -- ICES
+        WHEN c.fao_area_id = 27
+        THEN (SELECT eez_ices_combo_id
+              FROM sedna.eez_ices_combo eic
+              WHERE eic.eez_id = c.eez_id
+                AND eic.ices_area_id = ia.ices_area
+                AND eic.is_ifa = (st.sector_type_id != 1))
+        -- High Seas
+        WHEN c.eez_id = 0 AND c.fao_area_id > 0
+        THEN c.fao_area_id
+        -- IFA
+        WHEN c.eez_id > 0 AND c.fao_area_id > 0 AND c.sector_type_id != 1
+        THEN c.eez_id
+        -- EEZ
+        WHEN c.eez_id > 0 AND c.fao_area_id > 0 AND c.sector_type_id = 1
+        THEN c.eez_id
+        ELSE NULL -- this shouldn't happen
+    END AS area_id
 FROM sedna.catch c
 JOIN sedna.time y ON (y.year = c.year AND y.is_used_for_allocation)
 JOIN sedna.sector_type st ON (st.sector_type_id = c.sector_type_id)
