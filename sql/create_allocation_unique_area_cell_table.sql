2c109a1154a647ae66f97dafdfb8c8e1505df25b
EvilScott <reis.robert.s@gmail.com>
2022-08-23 10:20:00 -0700

    use the simple area mapper for the allocation_unique_area_cell table

--- sql/create_allocation_unique_area_cell_table.sql
+++ sql/create_allocation_unique_area_cell_table.sql
@@ -7,12 +7,27 @@ WITH (
   format = 'PARQUET',
   parquet_compression = 'SNAPPY'
 )
-AS SELECT aua.unique_area_id,
-          seca.allocation_simple_area_id,
+AS SELECT seca.allocation_simple_area_id,
           seca.cell_id,
           seca.water_area
-   FROM sedna.allocation_unique_area aua
-   JOIN sedna.simple_area_cell_assignment seca ON (aua.generic_allocation_area_id = seca.allocation_simple_area_id)
-   WHERE aua.allocation_area_type_id = 1;
-
---TODO allocation_area_type_id = 2 and 3 in UNIONS, type 2 uses AutoGen_HybridToSimpleAreaMapper and type 3 uses GetCellsForAreaType3
+FROM sedna.allocation_unique_area aua
+JOIN sedna.simple_area_cell_assignment seca
+ ON (aua.generic_allocation_area_id = seca.allocation_simple_area_id)
+WHERE aua.allocation_area_type_id = 1
+UNION ALL
+SELECT seca.allocation_simple_area_id,
+       seca.cell_id,
+       seca.water_area
+FROM sedna.allocation_unique_area aua
+JOIN sedna.hybrid_to_simple_area_mapper hsam
+  ON (aua.generic_allocation_area_id = hsam.hybrid_area_id)
+JOIN sedna.simple_area_cell_assignment seca
+  ON (hsam.contains_simple_area_id = seca.allocation_simple_area_id)
+WHERE aua.allocation_area_type_id = 2
+-- UNION ALL
+-- SELECT seca.allocation_simple_area_id,
+--        seca.cell_id,
+--        seca.water_area
+-- FROM sedna.allocation_unique_area aua
+-- TODO JOIN SOME STUFF
+-- WHERE aua.allocation_area_type_id = 3 --TODO type 3 uses GetCellsForAreaType3
